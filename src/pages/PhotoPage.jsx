8cda246e6907c005114e7afce5cbb1e47aa6dcb9
king-sawyer <kingsawyer555@gmail.com>
2025-08-17 09:59:44 -0700

    Started work on photo page

--- src/pages/PhotoPage.jsx
+++ src/pages/PhotoPage.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 const PhotoPage = () => {
+  const [imageSrc, setImageSrc] = useState(null);
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageSrc(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div>
-      <h1>PhotoPage</h1>
+      {imageSrc ? <img src={imageSrc} style={{ height: "200px" }} /> : <></>}
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
     </div>
   );
 };
