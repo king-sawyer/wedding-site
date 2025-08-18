ac69ff79364dcecd316e27eb8ca5b3c60e05a5f3
king-sawyer <kingsawyer555@gmail.com>
2025-08-17 22:00:57 -0700

    Updated theme

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,44 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ToastContainer, Bounce } from "react-toastify";
 
-const darkTheme = createTheme({
+// const darkTheme = createTheme({
+//   palette: {
+//     mode: "dark",
+//   },
+// });
+
+const weddingTheme = createTheme({
   palette: {
-    mode: "dark",
+    primary: {
+      light: "#8A9A5B",
+      main: "#8A9A5B",
+      dark: "#4A5D23",
+      contrastText: "#8A9A5B",
+    },
   },
 });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
+      {/* <ThemeProvider theme={darkTheme}> */}
+      <ThemeProvider theme={weddingTheme}>
         <App />
+        <ToastContainer
+          position="bottom-center"
+          autoClose={2500}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover={false}
+          theme="dark"
+          transition={Bounce}
+        />{" "}
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
