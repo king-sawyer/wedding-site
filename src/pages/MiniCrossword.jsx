4d89a4fa58185458ba87bb71cd51f77600511fe8
king-sawyer <kingsawyer555@gmail.com>
2025-08-21 21:21:24 -0700

    Mini maybe?

--- src/pages/MiniCrossword.jsx
+++ src/pages/MiniCrossword.jsx
@@ -1,9 +1,38 @@
-const MiniCrossword = () => {
-  return (
-    <div>
-      <h1>MiniCrossword</h1>
-    </div>
-  );
+import React from "react";
+
+import Crossword from "@jaredreisinger/react-crossword";
+
+const data = {
+  across: {
+    1: {
+      clue: "one plus one",
+      answer: "TWO",
+      row: 0,
+      col: 0,
+    },
+    3: {
+      clue: "one plus one",
+      answer: "TWO",
+      row: 2,
+      col: 0,
+    },
+  },
+  down: {
+    1: {
+      clue: "one plus one",
+      answer: "TWO",
+      row: 0,
+      col: 0,
+    },
+    2: {
+      clue: "three minus two",
+      answer: "ONE",
+      row: 0,
+      col: 2,
+    },
+  },
 };
 
-export default MiniCrossword;
+export default function MyPage() {
+  return <Crossword data={data} />;
+}
