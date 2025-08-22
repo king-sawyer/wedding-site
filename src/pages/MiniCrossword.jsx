cb56837c542e115a0ab14b8e26fe9ffd271e0213
king-sawyer <kingsawyer555@gmail.com>
2025-08-21 18:37:45 -0700

    Undo mini attempt :(

--- src/pages/MiniCrossword.jsx
+++ src/pages/MiniCrossword.jsx
@@ -1,36 +1,9 @@
-import Crossword from "@jaredreisinger/react-crossword";
-
-const data = {
-  across: {
-    1: {
-      clue: "one plus one",
-      answer: "TWO",
-      row: 0,
-      col: 0,
-    },
-    2: {
-      clue: "one plus one",
-      answer: "TWO",
-      row: 2,
-      col: 0,
-    },
-  },
-  down: {
-    1: {
-      clue: "three minus two",
-      answer: "ONE",
-      row: 0,
-      col: 0,
-    },
-    2: {
-      clue: "three minus two",
-      answer: "ONE",
-      row: 0,
-      col: 2,
-    },
-  },
+const MiniCrossword = () => {
+  return (
+    <div>
+      <h1>MiniCrossword</h1>
+    </div>
+  );
 };
 
-export default function MyPage() {
-  return <Crossword data={data} />;
-}
+export default MiniCrossword;
