3cbc56044185b3652fb0aaef8f5df876fab93a22
king-sawyer <kingsawyer555@gmail.com>
2025-08-22 22:27:46 -0700

    Word updates

--- src/pages/Welcomepage.jsx
+++ src/pages/Welcomepage.jsx
@@ -5,11 +5,11 @@ const Welcomepage = ({ logout, userData }) => {
     <div className="welcome-container">
       <h1>LET'S CELEBRATE!</h1>
       <h2>Hi {userData["first"]}, welcome to The King Wedding!</h2>
-      <p>To access the site, use the menu button in the top left corner.</p>
-      <h3>Be sure to try the following:</h3>
+      <p>Explore the site by using the menu button in the top left corner.</p>
+      <h3>Check out the following activities:</h3>
       <ul className="welcome-list">
         <li>Upload images throughout the night in the Photos tab</li>
-        <li>Send us a message in the Messages tab</li>
+        <li>Send us messages in the Messages tab</li>
         <li>See where you rank in the leaderboard</li>
         <li>Try the connections</li>
         <li>Try the wordle</li>
